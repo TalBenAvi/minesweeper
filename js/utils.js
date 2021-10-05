ed956e6b42d2c07441fc49cfbc1448d59609edeb
TalBenAvi <91826661+TalBenAvi@users.noreply.github.com>
2021-10-05 20:09:01 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,41 +1,34 @@
-function printMat(mat, selector) {
-    var strHTML = '<table><tbody>';
-    for (var i = 0; i < mat.length; i++) {
+'use strict'
+function renderBoard(board, selector) {
+    var strHTML = '<table>';
+    for (var i = 0; i < board.length; i++) {
         strHTML += '<tr>';
-        for (var j = 0; j < mat[0].length; j++) {
-            var cell = {
-                minesAroundCount: 0,
-                isShown: true,
-                isMine: false,
-                isMarked: false
-            };
-            if (gGame.shownCount === 0) {
-                gBoard = placeMine(gBoard, gLevel.MINES);
-                console.table(gBoard)
-                gGame.shownCount++;
-                var number = setMinesNegsCount(i, j);
+        for (var j = 0; j < board[0].length; j++) {
+            var cell = board[i][j];
+            var className="cell-${i}-${j}";
+            var cellTag = `<td class="${className}" oncontextmenu="cellMarked(this,${i},${j})" onclick="cellClicked(this,${i},${j})">`;
+            if (cell.isMine) strHTML += cellTag + `<span>${MINE}</span></td>`;
+            else {
+                if (cell.minesAroundCount === 0) {
+                    strHTML += cellTag + `<span></span></td>`;
+                } else {
+                    strHTML += cellTag + `<span>${cell.minesAroundCount}</span></td>`;
+                }
+
             }
-            if (mat[i][j] === MINE) cell.isMine = true;
-            else var number = setMinesNegsCount(i, j);
-            mat[i][j] = cell;
-            cell.minesAroundCount = number;
-            var className = 'cell cell' + i + '-' + j;
-            strHTML += `<td > <div class="${className}" onclick="cellClicked(${i},${j})"></div> </td>`;
         }
-        strHTML += '</tr class="' + className + '">'
+        strHTML += '</tr>';
     }
-    strHTML += '</tbody></table>';
-    var elContainer = document.querySelector(selector);
-    elContainer.innerHTML = strHTML;
+    strHTML += '</table>';
+    var elBoard = document.querySelector(selector);
+    elBoard.innerHTML = strHTML;
 }
-
-// location such as: {i: 2, j: 7}
-function renderCell(location, value) {
-    // Select the elCell and set the value
-    var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
-    elCell.innerHTML = value;
-}
-
-function getRandomIntInt(min, max) {
+// // location such as: {i: 2, j: 7}
+// function renderCell(location, value) {
+//     // Select the elCell and set the value
+//     var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
+//     elCell.innerHTML = value;
+// }
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
